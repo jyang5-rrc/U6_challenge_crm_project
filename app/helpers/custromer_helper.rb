6a52f7501fff3eba0cd14ef79ce4560d0226e8d7
jyang5 <jyang5@academic.rrc.ca>
2024-03-16 23:20:50 -0500

    using bulma to create a partial displaying customer information

---
+++ app/helpers/custromer_helper.rb
@@ -0,0 +1,2 @@
+module CustromerHelper
+end
---
+++ test/controllers/custromer_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CustromerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
